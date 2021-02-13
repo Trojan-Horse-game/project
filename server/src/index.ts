90de26a6746efbaf2a31c0cd6daf48907a884363
MrH <nicolas.audigie@hotmail.fr>
2021-02-13 17:19:01 +0000

    Demander le nombre de joueurs
    PremiÃre interraction avec la console

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,58 @@
-import "./pre-start"; // Must be the first import
+import "./pre-start";
 import app from "@server";
-import logger from "@shared/Logger";
+import log from "@shared/Logger";
+import readline from "readline";
+import { exit } from "process";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
 
-// Start the server
+// Output une question et attend un input
+function demander(quest: string): Promise<number> {
+  return new Promise((res) =>
+    rl.question(quest + "\n", (ans) => {
+      rl.close();
+      res(Number(ans));
+    })
+  );
+}
+
+// Vérifie que le nombre de joueurs est correct
+async function demanderNbJoueurs(): Promise<number> {
+  let joueursSet: boolean = false;
+  let nbJoueurs: number = 0;
+
+  while (!joueursSet) {
+    nbJoueurs = await demander("Combien de joueurs participeront ?");
+
+    if (isNaN(nbJoueurs)) console.log("Veuillez entrer un nombre valide !");
+    else if (nbJoueurs < 2)
+      console.log("Nombre de joueurs trop faible ! (Minimum 2)");
+    else if (nbJoueurs > 6)
+      console.log("Nombre de joueurs trop grand ! (Maximum 6)");
+    else joueursSet = true;
+  }
+  return nbJoueurs;
+}
+
+// Lance la partie en interraction dans la console
+async function lancerPartie(): Promise<void> {
+  let nbJoueurs: number = await demanderNbJoueurs();
+  log.info(nbJoueurs + " participent à la partie");
+
+  //Créer objet partie
+}
+
+// Démarre le server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-  logger.info("Express server started on port: " + port);
+let serveur = app.listen(port, async () => {
+  log.info("Express server started on port: " + port);
+  await lancerPartie();
+
+  serveur.close(() => {
+    log.info("Express stopped server");
+  });
+  exit(0);
 });
