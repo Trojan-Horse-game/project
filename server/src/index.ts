896fa1e519798a6e5ae78f92657f0e79653bbd38
MrH <nicolas.audigie@hotmail.fr>
2021-02-15 23:17:27 +0000

    Supprimer le logger restant

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,16 @@
 import "./pre-start";
 import app from "@server";
-import log from "@shared/Logger";
 import { exit } from "process";
 import { lancerPartie } from "./gamelogic/lancePartie";
 
 // Démarre le server
 const port = Number(process.env.PORT || 3000);
 let serveur = app.listen(port, async () => {
-  log.info("Express server started on port: " + port);
+  console.log("Express server started on port: " + port);
   await lancerPartie();
 
   serveur.close(() => {
-    log.info("Express stopped server");
+    console.log("Express stopped server");
   });
   exit(0);
 });
