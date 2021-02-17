7d3e4fa44413becf63c71f5c8430197e67eb675e
adtrevor <trevor.annedenise@icloud.com>
2021-02-17 04:26:03 +0100

    Remplacer les callbacks par await à certains endroits

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -10,63 +10,47 @@ import jwt from "jsonwebtoken";
 const usersRouter = Router();
 
 // GET users/
-usersRouter.get(
-  "/",
-  async function (req: Request, res: Response, next: NextFunction) {
-    createConnection()
-      .then(async (connection) => {
-        console.log("Loading users from the database...");
-        let users = await connection.manager.find(User);
-        console.log("Loaded users: ", users);
-      })
-      .catch((error) => console.log(error));
+usersRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    let connection = await createConnection();
+    console.log("Loading users from the database");
+    let users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+  } catch (error) {
+    console.error(error);
   }
-);
+});
 
 // GET users/:id
-usersRouter.get(
-  "/:id",
-  async function (req: Request, res: Response, next: NextFunction) {
-    createConnection()
-      .then(async (connection) => {
-        console.log("Loading users from the database...");
-        let user = await connection.manager.findOne(User, req.body.id);
-        console.log("Loaded user: ", user);
-      })
-      .catch((error) => console.log(error));
+usersRouter.get("/:id", async (req: Request, res: Response) => {
+  try {
+    let connection = await createConnection();
+    console.log("Loading users from the database...");
+    let user = await connection.manager.findOne(User, req.body.id);
+    console.log("Loaded user: ", user);
+  } catch (error) {
+    console.error(error);
   }
-);
+});
 
 // POST users/
-usersRouter.post(
-  "/signup",
-  async function (req: Request, res: Response, next: NextFunction) {
-    createConnection()
-      .then(async function (connection) {
-        let user = await connection.manager
-          .findOne(User, req.body.username)
-          .then(async function () {
-            if (user) {
-              return res.status(401).json({ error: "Username existe déjà !" });
-            }
-            console.log("Hashing a new user's password");
-            bcrypt
-              .hash(req.body.password, 10)
-              .then(async function (hash) {
-                console.log("Inserting a new user into the database...");
-                let user = new User();
-                user.username = req.body.email;
-                user.password = hash;
-                await connection.manager.save(user);
-                console.log("Saved a new user with id: " + user.id);
-              })
-              .catch((error) => console.log(error));
-          })
-          .catch((error) => console.log(error));
-      })
-      .catch((error) => console.log(error));
+usersRouter.post("/signup", async (req: Request, res: Response) => {
+  try {
+    let connection = await createConnection();
+    if (await connection.manager.findOne(User, req.body.username)) {
+      return res.status(401).json({ error: "Username existe déjà !" });
+    }
+    console.log("Hashing a new user's passwod");
+    let hash = await bcrypt.hash(req.body.password, 10);
+    let user = new User();
+    user.username = req.body.email;
+    user.password = hash;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+  } catch (error) {
+    console.error(error);
   }
-);
+});
 
 // PUT users/:id
 usersRouter.put(
