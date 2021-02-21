8c40c74516931ef2a4ea0536db64a6274ebb2ea7
zakariae <zakariae.baraat@etu.unistra.fr>
2021-02-21 12:57:44 +0100

    ajouter les routes http V3

--- server/src/entity/user.ts
+++ server/src/entity/user.ts
@@ -19,27 +19,19 @@ export class User {
     @Column()    
     wins!: number;
 
-<<<<<<< HEAD
-    @ManyToOne(() => friendship)
-    @JoinColumn()
-    friendship: friendship | undefined;  
+    
+    friendship: any;
 
+    
 }
 @Entity()
 export class friendship {
  
-
-    @Column()
-   public user_id1!: number;
-
-    @Column()
-    user_id2!:number;
+    @ManyToOne(() => User, (user1: User) => user1.friendship)
+    public user1_id: User | undefined;
+    @ManyToOne(() => User, (user2: User) => user2.friendship)
+    public user2_id: User | undefined;
 
     @Column()
     confirmed!:boolean;
-=======
-    @ManyToOne ( ( )  =>  User ,  (user: any)  =>  User . friend_id ) 
-    user : User | undefined ;
-    static friend_id: any;
->>>>>>> ae83f5ca049ca4076fdbc3cd64c2df89bca87beb
 }
