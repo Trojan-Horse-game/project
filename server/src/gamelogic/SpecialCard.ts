ae4e70a497ceb5a90bb8aedc81d185945dc24866
BASRI Youness <youness.basri@etu.unistra.fr>
2021-02-22 16:08:08 +0000

    début des actions spéciales des cartes (pas encore terminé)

--- server/src/gamelogic/SpecialCard.ts
+++ server/src/gamelogic/SpecialCard.ts
@@ -1,5 +1,7 @@
 import { Card, Color } from "./Card";
-
+import { State,GeneratorSlot } from "./GeneratorSlot";
+import { Action } from "./Action";
+import { Game } from "./Game";
 /* A class for the special action cards
 
    The color property is used to differentiate every action
@@ -11,8 +13,29 @@ export class SpecialCard implements Card {
     this.color = color;
   }
 
-  action(): void {
-    //TODO
+  action(game: Game, action: Action): void {
+    if (action.card.color === Color.Air) {
+      for (let index = 0; index < action.target.length; index++) {
+        game.discardHand([action.target[index]]);
+      }
+    }
+    if (action.card.color === Color.Water) {
+      //échange de toutes les cartes
+      [game.currentPlayer.base, game.players[action.target[0]].base] = [
+        game.players[action.target[0]].base,
+        game.currentPlayer.base,
+      ];
+    }
+    if (action.card.color === Color.Joker){
+      const indx = game.currentPlayer.getBase(action.slotTarget[0]);
+      const indx2 = game.currentPlayer.getBase(action.target[0]);
+      for (let i = 0; i < action.target.length; i +=2) {
+        game.currentPlayer.base[action.slotTarget[i+1]].addVirus(action.card);
+        
+      }
+    }
+
+    
   }
 
   toString(): string {
