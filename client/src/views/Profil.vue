1caa858fc75b94f3529456442575c8030fe3e9ca
Hakim ABDOUROIHAMANE <hakim_abdou@outlook.com>
2021-03-16 14:05:10 +0100

    Bugfix du menu des amities qui ne se fermait pas

--- client/src/views/Profil.vue
+++ client/src/views/Profil.vue
@@ -93,7 +93,7 @@
                       v-show="!this.showFriendInput"
                       ><v-icon>mdi-account-plus</v-icon></v-btn
                     >
-                    <v-btn text @click="addFriendDialog = false">
+                    <v-btn text @click="dialog = false">
                       Fermer
                     </v-btn>
                   </v-card-actions>
@@ -135,7 +135,7 @@
                   <v-divider></v-divider>
 
                   <v-card-actions>
-                    <v-btn text @click="addFriendDialog = false">
+                    <v-btn text @click="dialog = false">
                       Fermer
                     </v-btn>
                   </v-card-actions>
@@ -163,7 +163,6 @@ export default {
   data: () => ({
     dialog: false,
     tab: null,
-    addFriendDialog: false,
     newFriend: "",
     showFriendInput: false,
     validInput: false,
