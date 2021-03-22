c15c84d692839ad662b97a79c2cfef1f8264c636
MrH <nicolas.audigie@hotmail.fr>
2021-03-22 20:09:14 +0000

    Catch les erreur de findplayer et findgame

--- server/src/routes/game.routes.ts
+++ server/src/routes/game.routes.ts
@@ -73,24 +73,28 @@ module.exports = function (io: any) {
 
     // when the game begins
     socket.on("launch game", (roomId: string) => {
-      let thisgame = findGame(roomId, games);
-      thisgame.init();
-      for (let player of thisgame.players) {
-        io.to(player.socketid).emit("hand", player.hand);
-        io.in(thisgame.roomId).emit("base", player.base);
+      try {
+        let thisgame = findGame(roomId, games);
+        thisgame.init();
+        for (let player of thisgame.players) {
+          io.to(player.socketid).emit("hand", player.hand);
+          io.in(thisgame.roomId).emit("base", player.base);
+        }
+      } catch (err) {
+        io.to(socket.id).emit("oops", err);
       }
     });
 
     // when a user plays a card
     socket.on("play card", (roomId: string, action: Action) => {
-      let thisgame = findGame(roomId, games);
-      let player = findPlayer(socket.id, thisgame);
+      try {
+        let thisgame = findGame(roomId, games);
+        let player = findPlayer(socket.id, thisgame);
 
-      if (player !== thisgame.currentPlayer) {
-        let error = "Ce n'est pas le tour du joueur";
-        io.to(socket.id).emit("oops", error);
-      } else {
-        try {
+        if (player !== thisgame.currentPlayer) {
+          let error = "Ce n'est pas le tour du joueur";
+          io.to(socket.id).emit("oops", error);
+        } else {
           thisgame.checkAction(action);
           socket.to(roomId).emit("playCard", action);
 
@@ -98,22 +102,22 @@ module.exports = function (io: any) {
           io.to(socket.id).emit("cardPlayed", thisgame.currentPlayer.hand);
 
           io.in(roomId).emit("nextTurn", thisgame.currentPlayer);
-        } catch (err) {
-          io.to(socket.id).emit("wrongAction", err);
         }
+      } catch (err) {
+        io.to(socket.id).emit("oops", err);
       }
     });
 
     // when a user discard
     socket.on("discard", (roomId: string, indexDiscard: number[]) => {
-      let thisgame = findGame(roomId, games);
-      let player = findPlayer(socket.id, thisgame);
+      try {
+        let thisgame = findGame(roomId, games);
+        let player = findPlayer(socket.id, thisgame);
 
-      if (player !== thisgame.currentPlayer) {
-        let error = "Ce n'est pas le tour du joueur";
-        io.to(socket.id).emit("oops", error);
-      } else {
-        try {
+        if (player !== thisgame.currentPlayer) {
+          let error = "Ce n'est pas le tour du joueur";
+          io.to(socket.id).emit("oops", error);
+        } else {
           thisgame.checkDiscard(indexDiscard);
 
           let index: number;
@@ -129,33 +133,41 @@ module.exports = function (io: any) {
 
           thisgame.endTurn();
           io.in(roomId).emit("nextTurn", thisgame.currentPlayer);
-        } catch (err) {
-          io.to(socket.id).emit("oops", err);
         }
+      } catch (err) {
+        io.to(socket.id).emit("oops", err);
       }
     });
 
     // When a user sends a message on the chat
     socket.on("chat message", (roomId: string, msg: string) => {
-      let player = findPlayer(socket.id, findGame(roomId, games));
-      io.in(roomId).emit("chat message", player.pseudo, msg);
+      try {
+        let player = findPlayer(socket.id, findGame(roomId, games));
+        io.in(roomId).emit("chat message", player.pseudo, msg);
+      } catch (err) {
+        io.to(socket.id).emit("oops", err);
+      }
     });
 
     // When a user disconnects from the game
     socket.on("disconnecting", (_reason) => {
       for (const room of socket.rooms) {
         if (room == socket.id) {
-          let thisgame = findGame(room, games);
-          let player = findPlayer(socket.id, thisgame);
-          let idx = thisgame.players.indexOf(player);
+          try {
+            let thisgame = findGame(room, games);
+            let player = findPlayer(socket.id, thisgame);
+            let idx = thisgame.players.indexOf(player);
 
-          if (idx == thisgame.currentPlayerIdx) {
-            thisgame.resign();
-            socket.to(room).emit("disconnect", socket.id);
-            socket.to(room).emit("nextTurn", thisgame.currentPlayerIdx);
-          } else {
-            thisgame.resign(idx);
-            socket.to(room).emit("disconnect", socket.id);
+            if (idx == thisgame.currentPlayerIdx) {
+              thisgame.resign();
+              socket.to(room).emit("disconnect", socket.id);
+              socket.to(room).emit("nextTurn", thisgame.currentPlayerIdx);
+            } else {
+              thisgame.resign(idx);
+              socket.to(room).emit("disconnect", socket.id);
+            }
+          } catch (err) {
+            io.to(socket.id).emit("oops", err);
           }
         }
       }
@@ -163,7 +175,6 @@ module.exports = function (io: any) {
   });
 };
 
-// TODO: Try/catch findplayer et findgame
 //TODO: constructeur socketid et gameid
 //TODO: optimiser l'envoi de paquets
 // TODO : Je crois qu'on peut remplacer les io.to par socket.emit quand on renvoie au sender
