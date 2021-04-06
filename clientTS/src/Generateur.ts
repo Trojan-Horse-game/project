76148c4331b540dfda8526148f510b80fd1cfd7d
Youness BASRI <youness.basri@etu.unistra.fr>
2021-04-06 02:47:36 +0000

    fin du portage JS vers TS, (reste quelques petites modifs)

--- clientTS/src/Generateur.ts
+++ clientTS/src/Generateur.ts
@@ -1,7 +1,9 @@
 export default class Generateur {
   scene: any;
-  constructor(scene: any) {
+  index : number;
+  constructor(scene: any, index:number) {
     this.scene = scene;
+    this.index = index;
   }
   gen_render(x: number, y: number, sprite: any) {
     let generateur = this.scene.add
@@ -14,23 +16,26 @@ export default class Generateur {
   gen_renderZone(x: number, y: number, name: string) {
     let generateurDropZone = this.scene.add.zone(x, y).setCircleDropZone(35);
     generateurDropZone.setName(name);
-    generateurDropZone.setData({ state: "inactif" });
+    generateurDropZone.setData({ state: "inactif", zoneType: "generateur",idx: this.index});
     return generateurDropZone;
   }
 
   gen_renderOutline(generateurDropZone: any) {
     var generateurDropZoneOutline = this.scene.add.graphics();
     generateurDropZoneOutline.lineStyle(4, 0x999999);
+    generateurDropZoneOutline.setData({type : "generateur", idx: this.index});
     generateurDropZoneOutline.strokeCircle(
       generateurDropZone.x,
       generateurDropZone.y,
       generateurDropZone.input.hitArea.radius
     );
+
     return generateurDropZoneOutline;
   }
   displayGenerator(x: number, y: number, sprite: string, name: string) {
     this.gen_render(x, y, sprite);
     let foudre_renZ_1 = this.gen_renderZone(x, y, name);
-    this.gen_renderOutline(foudre_renZ_1);
+    let graphics = this.gen_renderOutline(foudre_renZ_1);
+    return graphics;
   }
 }
