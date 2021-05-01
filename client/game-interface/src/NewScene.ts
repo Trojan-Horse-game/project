e626d733eeb2d44fc27641d743074f44cc82fea4
adtrevor <trevor.annedenise@icloud.com>
2021-05-01 15:16:31 +0200

    Accepter des updates de joueurs avec un tableau

--- client/game-interface/src/NewScene.ts
+++ client/game-interface/src/NewScene.ts
@@ -6,9 +6,19 @@ export class ResponsiveScene extends Phaser.Scene {
   resize(width: number, height: number) {}
 }
 export class NewScene extends ResponsiveScene {
-  constructor() {
+  constructor(players: Player[], playerIndex: number) {
     super({});
+    this.players = players;
+    this.playerIndex = 0;
   }
+
+  players: Player[];
+  playerIndex: number;
+
+  positions: Position[];
+
+  playerSlot: PlayerSlot;
+  opponentsSlots: OpponentSlot[] = [];
 
   preload() {
     this.load.image("carte_verso", "src/assets/carte_verso.png");
@@ -28,26 +38,8 @@ export class NewScene extends ResponsiveScene {
     this.load.image("super_sign", "src/assets/super.png");
   }
 
-  player: PlayerSlot;
-  opponents: OpponentSlot[] = [];
-  playerCount = 6;
-
-  resize(width: number, height: number) {
-    this.updatePositions(width, height);
-    for (let i = 0; i < this.playerCount - 1; i++) {
-      let position = this.playerPosition(i);
-      this.opponents[i].setPosition(position.x, position.y);
-    }
-    this.player.setPosition(width / 2, height - 15 * window.devicePixelRatio);
-  }
-
-  playerPosition(playerIndex: number): { x: number; y: number } {
-    let index = NewScene.slotsMappings[this.playerCount][playerIndex];
-    return this.positions[index];
-  }
-
   create() {
-    let noms = ["Aghilas", "Siham", "Isa", "Nicolas", "Walid"];
+    let noms = ["Youness", "Aghilas", "Siham", "Isa", "Nicolas", "Walid"];
     let textures = [
       "fawkes_tete",
       "hutex_tete",
@@ -57,38 +49,98 @@ export class NewScene extends ResponsiveScene {
       "xmars_tete",
     ];
 
-    let profileRadius = 55 * window.devicePixelRatio;
-    this.player = new PlayerSlot(this, profileRadius, "Youness", "xmars_tete");
-    this.add.existing(this.player);
-
-    this.opponents = [];
-
-    for (let i = 0; i < this.playerCount - 1; i++) {
-      let player = new OpponentSlot(
-        this,
-        profileRadius,
-        NewScene.slotsLayouts[this.playerCount][i],
-        noms[i],
-        textures[i]
-      );
-      this.add.existing(player);
-      this.opponents.push(player);
+    let players: Player[] = [];
+    for (let i = 0; i < 3; i++) {
+      players.push(new Player(noms[i], textures[i]));
     }
+
+    this.updatePlayers(players, 0);
+
+    let profileRadius = 55 * window.devicePixelRatio;
+    this.playerSlot = new PlayerSlot(
+      this,
+      profileRadius,
+      this.players[this.playerIndex].name,
+      this.players[this.playerIndex].specie
+    );
+    this.add.existing(this.playerSlot);
 
     let width = this.cameras.main.width;
     let height = this.cameras.main.height;
     this.resize(width, height);
   }
 
-  positions: Position[];
+  resize(width: number, height: number) {
+    this.updateSlotsPositions(width, height);
+    console.log("Resize called");
+    console.log(this.opponentsSlots);
+    for (let i = 0; i < this.opponentsSlots.length; i++) {
+      let position = this.playerPosition(i);
+      console.log("Position", position);
+      this.opponentsSlots[i].setPosition(position.x, position.y);
+    }
+    this.playerSlot.setPosition(
+      width / 2,
+      height - 15 * window.devicePixelRatio
+    );
+  }
 
-  updatePositions(width: number, height: number) {
+  updatePlayers(newValue: Player[], playerIndex: number) {
+    this.players = newValue;
+    this.playerIndex = playerIndex;
+    this.createPlayers();
+  }
+
+  appendPlayer(newPlayer: Player) {}
+
+  removePlayer(removedPlayerIndex: number) {
+    if (removedPlayerIndex < this.playerIndex) {
+      this.playerIndex--;
+    }
+  }
+
+  createPlayers() {
+    // Remove any previously existing opponent slot
+    for (let opponentSlot of this.opponentsSlots) {
+      opponentSlot.destroy();
+    }
+    this.opponentsSlots = [];
+
+    // Create opponents
+    let index = (this.playerIndex + 1) % this.players.length;
+    let opponentIndex = 0;
+    while (index != this.playerIndex) {
+      let opponent = this.players[index];
+      let opponentSlot = new OpponentSlot(
+        this,
+        55 * window.devicePixelRatio,
+        this.slotLayoutForOpponent(opponentIndex),
+        opponent.name,
+        opponent.specie
+      );
+
+      this.opponentsSlots.push(opponentSlot);
+      this.add.existing(opponentSlot);
+      index = (index + 1) % this.players.length;
+      opponentIndex++;
+    }
+    console.log("Created slots", this.opponentsSlots);
+  }
+
+  slotLayoutForOpponent(opponentIndex: number): SlotLayout {
+    return NewScene.slotsLayouts[this.players.length][opponentIndex];
+  }
+
+  updateSlotsPositions(width: number, height: number) {
     let horizontalDistance = 100 * window.devicePixelRatio;
     let verticalDistance = 115 * window.devicePixelRatio;
     let heightDiff = 325 * window.devicePixelRatio;
     let minHeight = 400 * window.devicePixelRatio;
     this.positions = [
-      { x: horizontalDistance, y: Math.max(height - heightDiff, minHeight) },
+      {
+        x: horizontalDistance,
+        y: Math.max(height - heightDiff, minHeight),
+      },
       { x: horizontalDistance, y: verticalDistance },
       { x: width / 2, y: verticalDistance },
       { x: width - horizontalDistance, y: verticalDistance },
@@ -99,9 +151,14 @@ export class NewScene extends ResponsiveScene {
     ];
   }
 
+  playerPosition(playerIndex: number): { x: number; y: number } {
+    let index = NewScene.slotsMappings[this.players.length][playerIndex];
+    return this.positions[index];
+  }
+
   static slotsMappings = {
     2: [2],
-    3: [0, 4],
+    3: [1, 3],
     4: [0, 2, 4],
     5: [0, 1, 3, 4],
     6: [0, 1, 2, 3, 4],
@@ -109,7 +166,7 @@ export class NewScene extends ResponsiveScene {
 
   static slotsLayouts = {
     2: [SlotLayout.Middle],
-    3: [SlotLayout.BottomLeft, SlotLayout.BottomRight],
+    3: [SlotLayout.TopLeft, SlotLayout.TopRight],
     4: [SlotLayout.BottomLeft, SlotLayout.Middle, SlotLayout.BottomRight],
     5: [
       SlotLayout.BottomLeft,
@@ -128,3 +185,12 @@ export class NewScene extends ResponsiveScene {
 }
 
 type Position = { x: number; y: number };
+
+export class Player {
+  constructor(name: string, specie: string) {
+    this.name = name;
+    this.specie = specie;
+  }
+  name: string;
+  specie: string;
+}
