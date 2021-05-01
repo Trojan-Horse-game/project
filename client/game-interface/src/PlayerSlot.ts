c9e47b02af3d1c5f3e8d37dbdc554b0ef715a997
adtrevor <trevor.annedenise@icloud.com>
2021-05-01 10:00:06 +0200

    Refactorer PlayerSlot
    Intégrer le texte directement dans PlayerProfilePicture ce qui aidera à l'intégrer ailleurs nottament pour les éléments du joueur.

--- client/game-interface/src/PlayerSlot.ts
+++ client/game-interface/src/PlayerSlot.ts
@@ -13,23 +13,12 @@ export class PlayerSlot extends Phaser.GameObjects.Container {
     this.profilePicture = new PlayerProfilePicture(
       scene,
       playerCircleRadius,
-      pictureName
+      layoutInfo.textPosition,
+      pictureName,
+      name
     );
     this.add(this.profilePicture);
 
-    this.nameText = this.scene.add.text(0, 0, name.toUpperCase());
-    this.nameText.setOrigin(0.5, layoutInfo.textYOrigin);
-    this.nameText.setY(layoutInfo.textYPositionFactor * playerCircleRadius);
-    this.nameText.setFontSize(playerCircleRadius * 0.38);
-    this.nameText.setFontFamily("Gagalin");
-    let padding = playerCircleRadius * 0.0857;
-    this.nameText.setPadding(padding, 0, padding, padding);
-    this.nameText.style.setAlign("center");
-    this.nameText.style.setColor("FFFFFF");
-    this.nameText.style.setBackgroundColor("555455");
-    this.add(this.nameText);
-
-    // TODO: replace the circles with the actual generators
     let i = 0;
     for (let generatorKind in GeneratorKind) {
       let generatorInfo = layoutInfo.generatorRotationBuilder(i);
@@ -53,7 +42,6 @@ export class PlayerSlot extends Phaser.GameObjects.Container {
     }
   }
 
-  nameText: Phaser.GameObjects.Text;
   profilePicture: PlayerProfilePicture;
   generators = new Map<string, Generator>();
 
@@ -63,16 +51,17 @@ export class PlayerSlot extends Phaser.GameObjects.Container {
 
   set timerPercentage(newValue: number) {
     this.profilePicture.timerPercentage = newValue;
-    if (newValue == 0) {
-      this.nameText.setBackgroundColor("555455");
-    } else {
-      this.nameText.setBackgroundColor("0082FD");
-    }
   }
 }
 
 class PlayerProfilePicture extends Phaser.GameObjects.Container {
-  constructor(scene: Phaser.Scene, radius: number, pictureName: string) {
+  constructor(
+    scene: Phaser.Scene,
+    radius: number,
+    textPosition: TextPosition,
+    pictureName: string,
+    playerName: string
+  ) {
     super(scene);
     let strokeWidth = radius * 0.15;
 
@@ -100,9 +89,24 @@ class PlayerProfilePicture extends Phaser.GameObjects.Container {
     this.timerArc.closePath = false;
     this.timerArc.setStrokeStyle(strokeWidth, 0x0082fd, 1);
     this.add(this.timerArc);
+
+    // Create text
+    this.nameText = this.scene.add.text(0, 0, playerName.toUpperCase());
+    this.nameText.setOrigin(0.5, textPosition == TextPosition.Bottom ? 0 : 1);
+    let yTextPosFactor = textPosition == TextPosition.Top ? -1.15 : 1.15;
+    this.nameText.setY(yTextPosFactor * radius);
+    this.nameText.setFontSize(radius * 0.38);
+    this.nameText.setFontFamily("Gagalin");
+    let padding = radius * 0.0857;
+    this.nameText.setPadding(padding, 0, padding, padding);
+    this.nameText.style.setAlign("center");
+    this.nameText.style.setColor("FFFFFF");
+    this.nameText.style.setBackgroundColor("555455");
+    this.add(this.nameText);
   }
 
   timerArc: Phaser.GameObjects.Arc;
+  nameText: Phaser.GameObjects.Text;
 
   _timerPercentage: number = 0;
 
@@ -113,6 +117,11 @@ class PlayerProfilePicture extends Phaser.GameObjects.Container {
   set timerPercentage(newValue: number) {
     this._timerPercentage = newValue;
     this.timerArc.endAngle = -90 + 360 * newValue;
+    if (newValue == 0) {
+      this.nameText.setBackgroundColor("555455");
+    } else {
+      this.nameText.setBackgroundColor("0082FD");
+    }
   }
 }
 
@@ -150,7 +159,7 @@ class Generator extends Phaser.GameObjects.Container {
     this.add(this.superImage);
 
     // Generators begin with None state
-    this.setGeneratorState(GeneratorState.Enabled);
+    this.setGeneratorState(GeneratorState.None);
   }
 
   strokeWidth: number;
@@ -223,6 +232,11 @@ export enum SlotLayout {
   BottomRight,
 }
 
+export enum TextPosition {
+  Top,
+  Bottom,
+}
+
 /**
  * This class enables computing where elements should be positioned in
  * function of the given `slotLayout`.
@@ -235,40 +249,27 @@ class LayoutInfo {
   constructor(slotLayout: SlotLayout) {
     switch (slotLayout) {
       case SlotLayout.BottomLeft:
-        this.textYOrigin = 0;
-        this.textYPositionFactor = LayoutInfo.baseYPositionFactor;
-        this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(
-          -0.174,
-          0.62,
-          4
-        );
+        this.textPosition = TextPosition.Bottom;
+        this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(-0.5, 0.62);
         break;
 
       case SlotLayout.BottomRight:
-        this.textYOrigin = 0;
-        this.textYPositionFactor = LayoutInfo.baseYPositionFactor;
-        this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(
-          -2.306,
-          0.62,
-          0
-        );
+        this.textPosition = TextPosition.Bottom;
+        this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(-2.0, 0.62);
         break;
 
       case SlotLayout.TopLeft:
-        this.textYOrigin = 1;
-        this.textYPositionFactor = -LayoutInfo.baseYPositionFactor;
+        this.textPosition = TextPosition.Top;
         this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(0.875, 0.7);
         break;
 
       case SlotLayout.TopRight:
-        this.textYOrigin = 1;
-        this.textYPositionFactor = -LayoutInfo.baseYPositionFactor;
+        this.textPosition = TextPosition.Top;
         this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(-3.675, 0.7);
         break;
 
       case SlotLayout.Middle:
-        this.textYOrigin = 1;
-        this.textYPositionFactor = -LayoutInfo.baseYPositionFactor;
+        this.textPosition = TextPosition.Top;
         this.generatorRotationBuilder = LayoutInfo.makeGenPosBuild(
           1.3089,
           0.9162
@@ -277,8 +278,7 @@ class LayoutInfo {
     }
   }
 
-  textYOrigin: number;
-  textYPositionFactor: number;
+  textPosition: TextPosition;
   generatorRotationBuilder: (index: number) => GeneratorPosition;
 
   static baseYPositionFactor: number = 1.15;
@@ -297,10 +297,6 @@ class LayoutInfo {
         return new GeneratorPosition(last.rotation, 1.05);
       }
     };
-  }
-
-  static deg2rad(degrees: number): number {
-    return (degrees * Math.PI) / 180;
   }
 }
 
