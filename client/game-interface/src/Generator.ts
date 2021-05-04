98687cd93a849778539bb7e456860772331336a8
Trevör <817-adtrevor@users.noreply.git.unistra.fr>
2021-05-04 13:23:23 +0000

    Card drag and drop

--- client/game-interface/src/Generator.ts
+++ client/game-interface/src/Generator.ts
@@ -1,5 +1,11 @@
 import "phaser";
-import { GeneratorKind } from "./Card";
+import {
+  GeneratorKind,
+  Card,
+  GeneratorCard,
+  ActionCard,
+  GeneratorCardKind
+} from "./Card";
 
 export enum GeneratorState {
   None,
@@ -13,10 +19,13 @@ export class Generator extends Phaser.GameObjects.Container {
   constructor(
     scene: Phaser.Scene,
     radius: number,
-    generatorKind: GeneratorKind
+    generatorKind: GeneratorKind,
+    playerGenerator: boolean
   ) {
     super(scene);
     this.strokeWidth = radius * 0.14;
+    this.generatorKind = generatorKind;
+    this.playerGenerator = playerGenerator;
 
     // Make and add background circle
     this.backgroundCircle = this.scene.add.circle(0, 0, radius);
@@ -44,8 +53,16 @@ export class Generator extends Phaser.GameObjects.Container {
 
     // Generators begin with None state
     this.setGeneratorState(GeneratorState.None);
+
+    this.setSize(radius * 2, radius * 2);
+    this.setInteractive();
+    this.input.dropZone = true;
   }
 
+  generatorState: GeneratorState;
+  isSuper: boolean = false;
+  generatorKind: GeneratorKind;
+  playerGenerator: boolean;
   strokeWidth: number;
   backgroundCircle: Phaser.GameObjects.Arc;
   border: Phaser.GameObjects.Arc;
@@ -53,6 +70,16 @@ export class Generator extends Phaser.GameObjects.Container {
   superImage: Phaser.GameObjects.Image;
 
   setGeneratorState(state: GeneratorState, isSuper = false) {
+    this.generatorState = state;
+    this.isSuper = isSuper;
+    this.setGeneratorDisplayState(state, isSuper);
+  }
+
+  resetDisplayState() {
+    this.setGeneratorDisplayState(this.generatorState, this.isSuper);
+  }
+
+  setGeneratorDisplayState(state: GeneratorState, isSuper = false) {
     if (isSuper) {
       this.superImage.setAlpha(1);
     } else {
@@ -78,7 +105,7 @@ export class Generator extends Phaser.GameObjects.Container {
         break;
 
       case GeneratorState.AlwaysProtected:
-        this.backgroundCircle.setFillStyle(0x84f214, 0.5);
+        this.backgroundCircle.setFillStyle(0x84f214, 0.75);
         this.border.setStrokeStyle(this.strokeWidth, 0x84f214, 1);
         this.generatorImage.setAlpha(1);
         break;
@@ -88,6 +115,86 @@ export class Generator extends Phaser.GameObjects.Container {
         this.border.setStrokeStyle(this.strokeWidth, 0xe61515, 1);
         this.generatorImage.setAlpha(1);
         break;
+    }
+  }
+
+  nextState(card: Card): [GeneratorState, boolean] {
+    if (card instanceof ActionCard) {
+      return [this.generatorState, this.isSuper];
+    } else if (card instanceof GeneratorCard) {
+      if (
+        card.generator == this.generatorKind ||
+        ((this.isSuper || card.generator == GeneratorKind.Joker) &&
+          card.kind != GeneratorCardKind.Generator)
+      ) {
+        switch (this.generatorState) {
+          case GeneratorState.None:
+            if (
+              card.kind == GeneratorCardKind.Generator &&
+              this.playerGenerator
+            ) {
+              return [GeneratorState.Enabled, false];
+            }
+            break;
+
+          case GeneratorState.Enabled:
+            if (
+              this.playerGenerator &&
+              card.kind == GeneratorCardKind.Medicine
+            ) {
+              return [
+                GeneratorState.Protected,
+                card.generator == GeneratorKind.Joker
+              ];
+            } else if (
+              !this.playerGenerator &&
+              card.kind == GeneratorCardKind.Virus
+            ) {
+              return [
+                GeneratorState.Attacked,
+                card.generator == GeneratorKind.Joker
+              ];
+            }
+            break;
+
+          case GeneratorState.Protected:
+            if (
+              this.playerGenerator &&
+              card.kind == GeneratorCardKind.Medicine
+            ) {
+              return [GeneratorState.AlwaysProtected, false];
+            } else if (
+              !this.playerGenerator &&
+              card.kind == GeneratorCardKind.Virus
+            ) {
+              return [
+                GeneratorState.Enabled,
+                card.generator == GeneratorKind.Joker
+              ];
+            }
+            break;
+
+          case GeneratorState.Attacked:
+            if (
+              this.playerGenerator &&
+              card.kind == GeneratorCardKind.Medicine
+            ) {
+              return [GeneratorState.Enabled, false];
+            } else if (
+              !this.playerGenerator &&
+              card.kind == GeneratorCardKind.Virus
+            ) {
+              return [GeneratorState.None, false];
+            }
+            break;
+
+          case GeneratorState.AlwaysProtected:
+            return [this.generatorState, this.isSuper];
+        }
+        return [this.generatorState, this.isSuper];
+      }
+    } else {
+      return [this.generatorState, this.isSuper];
     }
   }
 }
--- client/game-interface/src/PlayerSlot.ts
+++ client/game-interface/src/PlayerSlot.ts
@@ -1,10 +1,12 @@
 import "phaser";
 import { ProfilePicture, TextPosition } from "./ProfilePicture";
-import { Generator } from "./Generator";
+import { Generator, GeneratorState } from "./Generator";
 import { GeneratorKind, GeneratorCard, GeneratorCardKind } from "./Card";
 import { CardSprite } from "./CardSprite";
+import { MouseEventFSM } from "./MouseEventFSM";
 
 export class PlayerSlot extends Phaser.GameObjects.Container {
+
   constructor(
     scene: Phaser.Scene,
     profilePictureRadius: number,
@@ -12,22 +14,77 @@ export class PlayerSlot extends Phaser.GameObjects.Container {
     texture: string
   ) {
     super(scene);
-
     // Cards
     const ratio = 0.7069;
     const height = profilePictureRadius * 3.5;
     const width = height * ratio;
-    const margin = 10 * window.devicePixelRatio;
+    const margin = 20 * window.devicePixelRatio;
     const offsets = [-width - margin, 0, +width + margin];
     for (const offset of offsets) {
-      // let card = scene.add.sprite(0, 0, "carte_verso");
       const card = new CardSprite(
         scene,
-        new GeneratorCard(GeneratorCardKind.Medicine, GeneratorKind.Shield)
+        new GeneratorCard(GeneratorCardKind.Virus, GeneratorKind.Joker),
+        width,
+        height
       );
-      scene.input.setDraggable(card);
-      card.setDisplaySize(width, height);
-      card.setOrigin(0.5, 1);
+      card.selectedCallback = (selected: boolean) => {
+        if (selected) {
+          this.selectedCards.push(card);
+        } else {
+          const index = this.selectedCards.indexOf(card);
+          this.selectedCards.splice(index, 1);
+        }
+      };
+
+      card.on("dragstart", () => {
+        this.otherSelected = this.selectedCards.filter(value => {
+          return value !== card;
+        });
+        for (const otherCard of this.otherSelected) {
+          otherCard.startX = otherCard.x;
+          otherCard.startY = otherCard.y;
+        }
+      });
+
+      card.on("dragend", () => {
+        for (const otherCard of this.otherSelected) {
+          scene.tweens.add({
+            targets: otherCard,
+            x: otherCard.startX,
+            y: otherCard.startY,
+            alpha: 1,
+            scale: 1,
+            duration: 400,
+            ease: "power4"
+          });
+        }
+      });
+
+      card.on(
+        "drag",
+        (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
+          let xTranslation = pointer.x - pointer.downX;
+          let yTranslation = pointer.y - pointer.downY;
+          let distance = Math.sqrt(
+            Math.pow(xTranslation, 2) + Math.pow(yTranslation, 2)
+          );
+          // let minThreshold = 150 * window.devicePixelRatio;
+          let minThreshold = 0;
+          let proportion = Math.min(
+            1,
+            Math.max(0, distance - minThreshold) / 500
+          );
+          proportion = Math.min(proportion, 0.95);
+          for (const otherCard of this.otherSelected) {
+            let baseXPosition = otherCard.startX + xTranslation;
+            let xPosition =
+              (1 - proportion) * baseXPosition + proportion * card.x;
+            otherCard.setPosition(xPosition, card.y);
+            otherCard.setScale(card.scale);
+          }
+        }
+      );
+
       card.setX(offset);
       this.add(card);
       this.cards.push(card);
@@ -52,13 +109,17 @@ export class PlayerSlot extends Phaser.GameObjects.Container {
       const generator = new Generator(
         scene,
         generatorRadius,
-        GeneratorKind[generatorKind]
+        GeneratorKind[generatorKind],
+        true
       );
       generator.setPosition(offset, -height * 1.3);
+      generator.setGeneratorState(GeneratorState.Enabled);
       offset += increment;
       this.add(generator);
     }
   }
 
+  private otherSelected: CardSprite[] = [];
+  selectedCards: CardSprite[] = [];
   cards: CardSprite[] = [];
 }
