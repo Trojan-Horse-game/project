b13e7335b86b8f603fd7dd52fff0d83b0e9e97f4
Hakim ABDOUROIHAMANE <hakim_abdou@outlook.com>
2021-05-05 03:26:45 +0200

    Bug de lancement resolu ??

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  devServer: {
+    clientLogLevel: "info"
+  },
   transpileDependencies: ["vuetify"],
   publicPath:
     process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/"
