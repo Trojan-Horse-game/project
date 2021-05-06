342a002ba28a7af3b5c813bd112d3187c96cf53c
adtrevor <trevor.annedenise@icloud.com>
2021-05-06 15:11:13 +0200

    Misc improvements

--- client/game-interface/src/GameScene.ts
+++ client/game-interface/src/GameScene.ts
@@ -1,151 +1,255 @@
-import { Card } from "./Card";
-import { GameSceneDelegate, Species } from "./GameSceneDelegate";
-import { io, Socket } from "socket.io-client";
-import { NewScene, Player } from "./NewScene";
+import "phaser";
+import { OpponentSlot, SlotLayout } from "./OpponentSlot";
+import { PlayerSlot } from "./PlayerSlot";
+import { Card, ActionCardKind, GeneratorKind, GeneratorCardKind } from "./Card";
+import { GameSceneDelegate } from "./GameSceneDelegate";
+import { ProfilePicture } from "./ProfilePicture";
 
-export class GameScene implements GameSceneDelegate {
-  private socket: Socket;
-  scene: NewScene;
-  players: Player[] = [];
-  playersCards: Card[] = [];
-  availableSpecies: Species[] = [];
-  room: string;
-  playerIndex: number;
-  winnerIndex: number;
-
-  constructor() {
-    this.scene = new NewScene(this.players, this.players.length);
-    this.socket = io("localhost:3000");
-
-    this.socket.on("oops", (err) => {
-      console.log(err);
-    });
-
-    this.socket.on("hand", (hand) => {
-      this.playersCards = hand;
-    });
-
-    this.socket.on("base", (base) => {});
-
-    this.socket.on("next turn", (playerIndex) => {
-      this.playerIndex = playerIndex;
-    });
-
-    this.socket.on("play card", (action: Action) => {
-      let opponent = action.target[0];
-      let slotTarget = action.slotTarget[0];
-      // Voir avec trevor
-    });
-
-    this.socket.on("discard", (indexDiscard, cards) => {
-      // Voir avec trevor
-    });
-
-    this.socket.on("leave game", (playerIdx) => {
-      this.scene.removePlayer(playerIdx);
-    });
-
-    this.socket.on("join game", (pseudo, specie) => {
-      let player = new Player(pseudo, specie);
-      this.scene.appendPlayer(player);
-    });
-
-    this.socket.on("end game", (winner) => {
-      this.winnerIndex = winner;
-    });
-
-    this.socket.on("game id", (roomId) => {
-      this.room = roomId;
-    });
-
-    this.socket.on("available species", (availableSpecies) => {
-      this.availableSpecies = availableSpecies;
-    });
-
-    this.socket.on("players", (pseudo, species) => {
-      let player = new Player(pseudo, species);
-      this.scene.appendPlayer(player);
-    });
-  }
-
-  didDropCard(
-    card: Card,
-    cardIndex: number,
-    playerIndex: number,
-    generatorIndex: number
-  ) {
-    try {
-      let action = new Action(card, cardIndex);
-      action.addSlotTarget(generatorIndex);
-      action.addTarget(playerIndex);
-      this.socket.emit("play card", this.room, action);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  didDiscard(cardsIndices: number[]) {
-    try {
-      this.socket.emit("discard", this.room, cardsIndices);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  launchGame(roomId: string) {
-    try {
-      this.socket.emit("launch game", this.room);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  createGame(pseudo: string, specie: Species) {
-    try {
-      this.socket.emit("create game", pseudo, specie);
-      // specie index or string ??
-      let player = new Player(pseudo, specie);
-      this.players.push(player);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  joinGame(pseudo: string, roomId: string) {
-    try {
-      this.socket.emit("join game", pseudo, roomId);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  chooseSpecie(specie: Species) {
-    try {
-      this.socket.emit("choose species", specie);
-    } catch (err) {
-      console.log(err);
-    }
+export class ResponsiveScene extends Phaser.Scene {
+  resize(width: number, height: number) {
+    console.error("Method must be overriden");
   }
 }
-
-export class Action {
-  card: Card;
-  indexInHand: number;
-  target: number[] = [];
-  slotTarget: number[] = [];
-
-  constructor(card: Card, indexInHand: number) {
-    this.card = card;
-    this.indexInHand = indexInHand;
+export class GameScene extends ResponsiveScene {
+  constructor(currentPlayer: Player) {
+    super({});
+    this.players = [currentPlayer];
+    this.playerIndex = 0;
+    this.currentPlayer = 0;
   }
 
-  /* Add a target's index to the action */
-  addTarget(target: number) {
-    this.target.push(target);
+  players: Player[];
+  playerIndex: number;
+  currentPlayer: number;
+
+  positions: Position[];
+
+  playerSlot: PlayerSlot;
+  opponentsSlots: OpponentSlot[] = [];
+
+  delegate: GameSceneDelegate;
+
+  preload() {
+    this.load.image("carte_verso", "src/assets/carte_verso.png");
+
+    this.load.image("fawkes_tete", "src/assets/Fawkes_tete.png");
+    this.load.image("hutex_tete", "src/assets/Hutex_tete.png");
+    this.load.image("robotec_tete", "src/assets/Robotec_tete.png");
+    this.load.image("spectre_tete", "src/assets/Spectre_tete.png");
+    this.load.image("totox_tete", "src/assets/Totox_tete.png");
+    this.load.image("xmars_tete", "src/assets/Xmars_tete.png");
+
+    this.load.image("electricity", "src/assets/foudre_log.png");
+    this.load.image("air", "src/assets/air_log.png");
+    this.load.image("water", "src/assets/goute_log.png");
+    this.load.image("shield", "src/assets/radiation_log.png");
+    this.load.image("joker", "src/assets/super_log.png");
+    this.load.image("super_sign", "src/assets/super.png");
+
+    // Action cards assets
+    for (let actionName in ActionCardKind) {
+      this.load.image(
+        ActionCardKind[actionName],
+        "src/assets/" + ActionCardKind[actionName] + ".jpg"
+      );
+    }
+
+    // Generator card assets
+    let suffixes = ["G", "P", "V"];
+    for (let suffix of suffixes) {
+      for (let generatorName in GeneratorKind) {
+        this.load.image(
+          GeneratorKind[generatorName] + "_" + suffix,
+          "src/assets/" + GeneratorKind[generatorName] + "_" + suffix + ".jpg"
+        );
+      }
+    }
   }
 
-  /* add a BaseSlot's indexx target to the action */
-  addSlotTarget(slot: number) {
-    this.slotTarget.push(slot);
+  create() {
+    const profileRadius = 55 * window.devicePixelRatio;
+    this.playerSlot = new PlayerSlot(
+      this,
+      profileRadius,
+      this.players[this.playerIndex].name,
+      this.players[this.playerIndex].specie
+    );
+    this.add.existing(this.playerSlot);
+
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    this.resize(width, height);
+    this.nextTurn(0);
   }
+
+  resize(width: number, height: number) {
+    this.updateSlotsPositions(width, height);
+    for (let i = 0; i < this.opponentsSlots.length; i++) {
+      const position = this.playerPosition(i);
+      this.opponentsSlots[i].setPosition(position.x, position.y);
+    }
+    this.playerSlot.setPosition(
+      width / 2,
+      height - 15 * window.devicePixelRatio
+    );
+  }
+
+  updatePlayers(newValue: Player[], playerIndex: number) {
+    this.players = newValue;
+    this.playerIndex = playerIndex;
+    this.createPlayers();
+  }
+
+  appendPlayer(newPlayer: Player) {
+    this.players.push(newPlayer);
+    this.createPlayers();
+  }
+
+  removePlayer(removedPlayerIndex: number) {
+    if (removedPlayerIndex < this.playerIndex) {
+      this.playerIndex--;
+    }
+    this.players.splice(removedPlayerIndex, 1);
+    this.createPlayers();
+  }
+
+  nextTurn(nextPlayer: number) {
+    let nextOpponentSlotIndex =
+      nextPlayer > this.playerIndex ? nextPlayer - 1 : nextPlayer;
+    console.log("Hey");
+
+    if (this.currentPlayer) {
+      let opponentSlotIndex =
+        this.currentPlayer > this.playerIndex
+          ? this.currentPlayer - 1
+          : this.currentPlayer;
+      // If current player is the user
+      if (this.currentPlayer == this.playerIndex) {
+        this.playerSlot.profilePicture.timerPercentage = 0;
+        this.playerSlot.playerInteractive = false;
+      } else {
+        this.opponentsSlots[
+          opponentSlotIndex
+        ].profilePicture.timerPercentage = 0;
+      }
+    }
+
+    let nextPlayerProfilePicture: ProfilePicture;
+    if (nextPlayer == this.playerIndex) {
+      nextPlayerProfilePicture = this.playerSlot.profilePicture;
+      this.playerSlot.playerInteractive = true;
+    } else {
+      nextPlayerProfilePicture = this.opponentsSlots[nextOpponentSlotIndex]
+        .profilePicture;
+    }
+    nextPlayerProfilePicture.timerPercentage = 1;
+    let duration = 20000;
+    let fps = 30;
+    let increments = 1 / (duration / 1000) / fps;
+    let timerUpdater = window.setInterval(() => {
+      let current = nextPlayerProfilePicture.timerPercentage;
+      let next = Math.max(current - increments, 0);
+      nextPlayerProfilePicture.timerPercentage = next;
+    }, 1000 / fps);
+    window.setTimeout(() => {
+      clearInterval(timerUpdater);
+    }, duration + 50);
+    this.currentPlayer = nextPlayer;
+  }
+
+  distributeCards(cards: Card[]) {}
+
+  createPlayers() {
+    // Remove any previously existing opponent slot
+    for (const opponentSlot of this.opponentsSlots) {
+      opponentSlot.destroy();
+    }
+    this.opponentsSlots = [];
+
+    // Create opponents
+    let index = (this.playerIndex + 1) % this.players.length;
+    let opponentIndex = 0;
+    while (index != this.playerIndex) {
+      const opponent = this.players[index];
+      const opponentSlot = new OpponentSlot(
+        this,
+        55 * window.devicePixelRatio,
+        this.slotLayoutForOpponent(opponentIndex),
+        opponent.name,
+        opponent.specie
+      );
+
+      this.opponentsSlots.push(opponentSlot);
+      this.add.existing(opponentSlot);
+      index = (index + 1) % this.players.length;
+      opponentIndex++;
+    }
+  }
+
+  slotLayoutForOpponent(opponentIndex: number): SlotLayout {
+    return GameScene.slotsLayouts[this.players.length][opponentIndex];
+  }
+
+  updateSlotsPositions(width: number, height: number) {
+    const horizontalDistance = 100 * window.devicePixelRatio;
+    const verticalDistance = 115 * window.devicePixelRatio;
+    const heightDiff = 325 * window.devicePixelRatio;
+    const minHeight = 400 * window.devicePixelRatio;
+    this.positions = [
+      {
+        x: horizontalDistance,
+        y: Math.max(height - heightDiff, minHeight)
+      },
+      { x: horizontalDistance, y: verticalDistance },
+      { x: width / 2, y: verticalDistance },
+      { x: width - horizontalDistance, y: verticalDistance },
+      {
+        x: width - horizontalDistance,
+        y: Math.max(height - heightDiff, minHeight)
+      }
+    ];
+  }
+
+  playerPosition(playerIndex: number): { x: number; y: number } {
+    let index = GameScene.slotsMappings[this.players.length][playerIndex];
+    return this.positions[index];
+  }
+
+  static slotsMappings = {
+    2: [2],
+    3: [1, 3],
+    4: [0, 2, 4],
+    5: [0, 1, 3, 4],
+    6: [0, 1, 2, 3, 4]
+  };
+
+  static slotsLayouts = {
+    2: [SlotLayout.Middle],
+    3: [SlotLayout.TopLeft, SlotLayout.TopRight],
+    4: [SlotLayout.BottomLeft, SlotLayout.Middle, SlotLayout.BottomRight],
+    5: [
+      SlotLayout.BottomLeft,
+      SlotLayout.TopLeft,
+      SlotLayout.TopRight,
+      SlotLayout.BottomRight
+    ],
+    6: [
+      SlotLayout.BottomLeft,
+      SlotLayout.TopLeft,
+      SlotLayout.Middle,
+      SlotLayout.TopRight,
+      SlotLayout.BottomRight
+    ]
+  };
+}
+
+type Position = { x: number; y: number };
+
+export class Player {
+  constructor(name: string, specie: string) {
+    this.name = name;
+    this.specie = specie;
+  }
+  name: string;
+  specie: string;
 }
