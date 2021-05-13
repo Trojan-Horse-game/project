062f27b7b24e910751bc58dd505fd7ebeea160c9
Sihamais <sihamais98@gmail.com>
2021-05-13 03:56:28 +0200

    Corriger le catch

--- server/src/routes/game.routes.ts
+++ server/src/routes/game.routes.ts
@@ -328,6 +328,7 @@ module.exports = function (io: any) {
 
     // When a user disconnects from the game
     socket.on("disconnecting", (_reason) => {
+      console.log(_reason);
       for (const room of socket.rooms) {
         if (room !== socket.id) {
           forfeit(io, room, socket);
