df052d74f7a98bfbea767938df2fe5863f4f4021
Sihamais <sihamais98@gmail.com>
2021-05-16 03:30:40 +0200

    Résoudre le bug du tour

--- server/src/routes/game.routes.ts
+++ server/src/routes/game.routes.ts
@@ -68,7 +68,7 @@ function forfeit(io: any, room: string, playerSocket: Socket) {
 
       if (!thisgame.inProgress) {
         io.in(room).emit("endGame", thisgame.winnerIdx);
-        clearTimeout(nextTurnTimeout)
+        clearTimeout(nextTurnTimeout);
         games.splice(games.indexOf(thisgame), 1);
       }
     }
@@ -94,12 +94,11 @@ function nextTurn(io: any, thisGame: Game) {
       });
     }
   } while (thisGame.currentPlayer.hand.length === 0);
-  if(thisGame.inProgress){
+  if (thisGame.inProgress) {
     nextTurnTimeout = setTimeout(() => {
       nextTurn(io, thisGame);
     }, 20000);
-  }
-  else{
+  } else {
     clearTimeout(nextTurnTimeout);
   }
 }
@@ -129,11 +128,11 @@ module.exports = function (io: any) {
     // When creating a new game
     socket.on("create game", (pseudo: string) => {
       try {
-          let count = howManyGames(pseudo, games);
-          if (count > 1) {
-            socket.emit("closeTab");
-            throw "Already in a game !";
-          }
+        let count = howManyGames(pseudo, games);
+        if (count > 1) {
+          socket.emit("closeTab");
+          throw "Already in a game !";
+        }
         const room = "ROOM-" + socket.id;
         socket.join(room);
         let game = new Game(room);
@@ -271,13 +270,7 @@ module.exports = function (io: any) {
             thisgame.checkAction(action);
             socket.emit("valid");
             socket.to(roomId).emit("playCard", action);
-
             thisgame.playAction(action);
-            const kinds = cardsKinds(thisgame.currentPlayer.hand);
-            socket.emit("hand", {
-              hand: thisgame.currentPlayer.hand,
-              kind: kinds,
-            });
 
             thisgame.players.forEach((player, index) => {
               io.in(thisgame.roomId).emit("base", {
@@ -286,11 +279,17 @@ module.exports = function (io: any) {
               });
             });
 
-            console.log("in progress", thisgame.inProgress)
+            const currentPlayerIdxCopy = thisgame.currentPlayerIdx;
+            console.log("in progress", thisgame.inProgress);
             if (thisgame.inProgress) {
               nextTurn(io, thisgame);
+              const lastPlayer = thisgame.players[currentPlayerIdxCopy];
+              io.to(lastPlayer.socketId).emit("hand", {
+                hand: lastPlayer.hand,
+                kind: cardsKinds(lastPlayer.hand),
+              });
             } else {
-              clearTimeout(nextTurnTimeout)
+              clearTimeout(nextTurnTimeout);
               io.in(roomId).emit("endGame", thisgame.winnerIdx);
             }
           }
@@ -315,7 +314,7 @@ module.exports = function (io: any) {
             throw "Not your turn !";
           } else {
             thisgame.checkDiscard(indexDiscard);
-            socket.emit("valid")
+            socket.emit("valid");
 
             let index: number;
             let cards: Card[] = [];
@@ -369,7 +368,7 @@ module.exports = function (io: any) {
 
     socket.on("leave game", (roomId: string) => {
       socket.leave(roomId);
-    })
+    });
 
     socket.on("gameState", (pseudo: string) => {
       let count = howManyGames(pseudo, games);
